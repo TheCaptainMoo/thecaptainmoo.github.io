1f09f197828be3e56bf71fb5d7af5e95de9ab371
TheCaptainMoo <59519774+TheCaptainMoo@users.noreply.github.com>
2021-06-22 18:30:09 +1000

    Website V1

---
+++ script1.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+function $(id){
+	return document.getElementById(id);
+}
+
+
+function randomnumber(x){
+	return Math.floor(Math.random()*x + 1);
+}
+
+function main(){
+	
+}
+
+
+
+window.onload = main;
