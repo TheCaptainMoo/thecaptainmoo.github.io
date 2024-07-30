1d1f62170efe5aa54215b4f549b94d53e244a6b2
TheCaptainMoo <thecaptainmoo@outlook.com>
2024-07-30 19:25:29 +1000

    Updated website
    Website now uses Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: { 
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./public/**/*.{html,js}']
+  },
+  theme: {
+    extend: {
+      colors: {
+        "orange": "#ff7700",
+        "grey": "#2e2f2f",
+        "white": "#eff1f3",
+        "blue2" : "#3772ff"
+      },
+      fontFamily: {
+        "body": ["Roboto", "sans-serif"]
+      },
+    },
+  },
+  plugins: [
+    require('taos/plugin')
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ]
+}
+
